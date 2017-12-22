4831d3495cab0b20ddcc957ad0d3d6a9db750817
oliverpople <oliverpople@gmail.com>
2017-12-22 08:24:02 +0000

    Text now renders on index.html

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// create a new component.  This component
+// should produce some html
+const App = function() {
+  return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML
+// and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));  // second argument is the target container/ DOM node
