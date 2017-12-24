45677666269fa903dd0dd0656dfa3d974da92017
oliverpople <oliverpople@gmail.com>
2017-12-24 07:16:28 +0000

    Added bootstrap classes and video property values to VideoDetail component. And crafted own embed url for each video (i.e. url + video/’s given ID). Also added a check to VideoDetail to see if video has been provided (returns ‘Loading’ if not). Added VideoDetail to index.js.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = '';
 
@@ -24,7 +25,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos= {this.state.videos} />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
